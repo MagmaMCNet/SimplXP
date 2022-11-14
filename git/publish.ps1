6e2aad8e6863b2ac72a2a905d5b30adb81c4944f
SMLkaiellis08 <kaicycle11@gmail.com>
2022-11-14 15:51:09 +0000

    Beta -> Host

--- git/publish.ps1
+++ git/publish.ps1
@@ -1,6 +1,7 @@
+Clear-Host
 git add .
 $m = Read-Host -Prompt 'commit Message'
 $Tag = Read-Host -Prompt 'Tag Name'
 git commit -m $m
 git tag -a R$Tag -m "git/publish.ps1"
-git push origin $Tag
+git push origin R$Tag
