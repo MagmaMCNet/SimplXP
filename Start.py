e2747196d872ea686a4e97d800237dd652ef56dc
MagmaMc <65583319+SMLkaiellis08@users.noreply.github.com>
2022-11-14 15:43:23 +0000

    Beta -> main

---
+++ Start.py
@@ -0,0 +1,47 @@
+import requests as Request
+import json, time, os, dotenv
+
+env_file = dotenv.find_dotenv()
+dotenv.load_dotenv(env_file)
+
+file_r = open("python.json", )
+data = json.load(file_r)
+file_r.close()
+if (data["Branch"]  == "Stable"):
+    print("Checking For Updates")
+    V = json.loads(str(Request.get(data["StableUrl"]+"/python.json").text))["Version"]
+    if (float(V) > float(data["Version"])):
+        print("Update Available", data["StableUrl"])
+        time.sleep(1.5)
+    else:
+        print("No Updates Found Starting As Normal")
+        time.sleep(1)
+elif (data["Branch"] == "beta"):
+    print("Checking For Updates")
+    V = json.loads(str(Request.get(data["BetaUrl"]+"/python.json").text))["Version"]
+    if (float(V) > float(data["Version"])):
+        print("Update Available", data["BetaUrl"])
+        time.sleep(1.5)
+    else:
+        print("No Updates Found Starting As Normal")
+        time.sleep(1)
+
+if (not data["HasRan"]):
+    os.system("cls")
+    time.sleep(0.5)
+    print("First Time Setup")
+    dotenv.set_key(env_file, "TOKEN", input("Bot Token: "))
+    dotenv.set_key(env_file, "XPMULTIPLIER", input("Xp Multipler [1, 2, ..]: "))
+    data["HasRan"] = True
+    
+    file = open("python.json", "w")
+    json.dump(data, file, indent=4)
+    file.close()
+    
+    os.system("cls")
+    time.sleep(0.5)
+    os.system("npm i")
+    time.sleep(0.5)
+    os.system("cls")
+
+os.system("npm run start")
---
+++ git/publish.ps1
@@ -0,0 +1,4 @@
+git add .
+$m = Read-Host -Prompt 'commit Message'
+git commit -m $m
+git push
